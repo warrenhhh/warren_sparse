67a9a60ab98a7be632a3cc6ee1dba096c5b2b5fd
Warren Huang <warrenhuang@WarrendeMacBook-Pro.local>
2023-11-16 17:48:49 -0600

    fix test sparse code for get

--- test_sparse_recommenderpy.py
+++ test_sparse_recommenderpy.py
@@ -1,5 +1,4 @@
-# csr_matrix
-# create a test sparse matrix
+# 在 test_sparse_recommender.py
 
 import pytest
 from sparse_recommender import sparse_matrix
@@ -11,18 +10,18 @@ def test_set():
     test_matrix.set(2, 2, 5)
     test_matrix.set(0, 0, 4)
 
-    assert test_matrix.get(1, 0) == 2
-    assert test_matrix.get(2, 2) == 5
+    assert test_matrix[1, 0] == 2
+    assert test_matrix[2, 2] == 5
 
 
 def test_recommend():
     test_recommend_matrix = sparse_matrix(5, 5)
     test_recommend_matrix.set(1, 1, 3)
     test_recommend_matrix.set(2, 2, 4)
-    test_recommend_matrix.set(1, 5, 1)
+    test_recommend_matrix.set(1, 4, 1)
     test_recommend_matrix.set(1, 1, 1)
     test_recommend_matrix.set(3, 3, 3)
-    test_recommend_matrix.get(1, 1, 1)
+    assert test_recommend_matrix[1, 1] == 1  # using assert to check this is correct
 
 
 def test_add_movie():
@@ -32,16 +31,18 @@ def test_add_movie():
     test_add_movie_matrix.set(0, 0, 2)
     test_add_movie_matrix.set(1, 3, 3)
     test_add_movie_matrix.set(1, 2, 2)
+    assert test_add_movie_matrix[1, 2] == 2  # using assert to check this is correct
 
 
 def test_to_dense():
     test_to_dense = sparse_matrix(5, 5)
     test_to_dense.set(1, 1, 3)
     test_to_dense.set(2, 2, 4)
-    test_to_dense.set(1, 5, 1)
+    test_to_dense.set(1, 4, 1)
     test_to_dense.set(1, 1, 1)
     test_to_dense.set(3, 3, 3)
-    test_to_dense.get(1, 1, 1)
+    dense_matrix = test_to_dense.to_dense()
+    assert dense_matrix[1, 1] == 1  # using assert to check this is correct
 
 
 def test_sparse_matrix():
@@ -51,3 +52,4 @@ def test_sparse_matrix():
     test_matrix.set(0, 0, 2)
     test_matrix.set(1, 3, 3)
     test_matrix.set(1, 2, 2)
+    assert test_matrix[1, 2] == 2  # using assert to check this is correct
