2cb37851d9b58cd73b5bd36d32f72f25085a0330
Warren Huang <warrenhuang@WarrendeMacBook-Pro.local>
2023-11-16 18:05:08 -0600

    fix test sparse code for matrix

--- sparse_recommender.py
+++ sparse_recommender.py
@@ -8,47 +8,43 @@ class sparse_matrix(csr_matrix):
         super().__init__((row, col), dtype=dtype)
         self.initialize_sparse_matrix()
     
-    def initialize_sparse_martix(self):
-        self.set = 0
-        self.get = {}
-        self.recommend = {}
-        self.add_movie = {}
-        self.to_dense = {}
-        pass
+    def initialize_sparse_matrix(self):
+        self.setmatrix = np.zeros((self.shape[0], self.shape[1]),
+                                  dtype=np.int32)
 
     def set(self, row, col, value):
-        if value != 0 and 0 <= row < self.initrow and 0 <= col < self.initcol:
-            sparse_matrix.setmatrix[row, col] = value
+        if value != 0 and 0 <= row < self.shape[0] and 0 <= col < self.shape[1]:
+            self.setmatrix[row, col] = value
         else:
             print("error")
 
     def get(self, row, col):
-        if 0 <= row < self.initrow and 0 <= col < self.initcol:
-            return sparse_matrix.getmatrix.get[row, col, 0]
+        if 0 <= row < self.shape[0] and 0 <= col < self.shape[1]:
+            return self.setmatrix[row, col]
         else:
             print("error")
 
     def recommend(self, user_vector):
         if self.shape[1] == user_vector.shape[0]:
-            recommendation_vector = csr_matrix
-            ((self.shape[0], user_vector.shape[1]))
-            for row_value in range(self.row):
-                for col_value in range(user_vector.col):
+            recommendation_vector = csr_matrix((self.shape[0], user_vector.shape[1]))
+            for row_value in range(self.shape[0]):
+                for col_value in range(user_vector.shape[1]):
                     result_value = 0
-                    for index in range(self.col):
-                        result_value = self.get(row_value, index) * user_vector.get(index, col_value)
-                        recommendation_vector.set(row_value, col_value, result_value)
+                    for index in range(self.shape[1]):
+                        result_value += self.get(row_value, index) * user_vector.get(index, col_value)
+                    recommendation_vector.set(row_value, col_value,
+                                              result_value)
             return recommendation_vector
         else:
             print("col is not the same with user_vector, error")
-    
+
     def add_movie(self, user_vector):
         if self.shape == user_vector.shape:
             recommendation_vector = csr_matrix((self.shape[0], user_vector.shape[1]))
-            for row_value in range(self.row):
-                for col_value in range(user_vector.col):
-                    new_value = self.get_value(row_value, col_value) + user_vector.get(row_value, col_value)
-                    recommendation_vector = csr_matrix(row_value, col_value, new_value)
+            for row_value in range(self.shape[0]):
+                for col_value in range(user_vector.shape[1]):
+                    new_value = self.get(row_value, col_value) + user_vector.get(row_value, col_value)
+                    recommendation_vector.set(row_value, col_value, new_value)
             return recommendation_vector
         else:
             print("row is not the same with user_vector, error")
@@ -56,14 +52,3 @@ class sparse_matrix(csr_matrix):
     def to_dense(self):
         dense_matrix = self.toarray()
         return dense_matrix
-
-    
-
-
-
-
-    
-
-
-
-
--- test_sparse_recommenderpy.py
+++ test_sparse_recommenderpy.py
@@ -21,7 +21,8 @@ def test_recommend():
     test_recommend_matrix.set(1, 4, 1)
     test_recommend_matrix.set(1, 1, 1)
     test_recommend_matrix.set(3, 3, 3)
-    assert test_recommend_matrix[1, 1] == 1  # using assert to check this is correct
+    assert test_recommend_matrix[1, 1] == 1
+    # using assert to check this is correct
 
 
 def test_add_movie():
@@ -31,7 +32,8 @@ def test_add_movie():
     test_add_movie_matrix.set(0, 0, 2)
     test_add_movie_matrix.set(1, 3, 3)
     test_add_movie_matrix.set(1, 2, 2)
-    assert test_add_movie_matrix[1, 2] == 2  # using assert to check this is correct
+    assert test_add_movie_matrix[1, 2] == 2
+    # using assert to check this is correct
 
 
 def test_to_dense():
@@ -42,7 +44,8 @@ def test_to_dense():
     test_to_dense.set(1, 1, 1)
     test_to_dense.set(3, 3, 3)
     dense_matrix = test_to_dense.to_dense()
-    assert dense_matrix[1, 1] == 1  # using assert to check this is correct
+    assert dense_matrix[1, 1] == 1
+    # using assert to check this is correct
 
 
 def test_sparse_matrix():
@@ -52,4 +55,5 @@ def test_sparse_matrix():
     test_matrix.set(0, 0, 2)
     test_matrix.set(1, 3, 3)
     test_matrix.set(1, 2, 2)
-    assert test_matrix[1, 2] == 2  # using assert to check this is correct
+    assert test_matrix[1, 2] == 2
+    # using assert to check this is correct
